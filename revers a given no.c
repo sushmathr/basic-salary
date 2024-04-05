1c3496042be778ce21b784800956daf77dc1cbdc
Sushmatha_Coder <141129916+sushmathr@users.noreply.github.com>
2024-04-05 15:44:51 +0530

    Add files via upload

---
+++ revers a given no.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int number, reversedNumber = 0, remainder;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    while (number != 0) {
+        remainder = number % 10;
+        reversedNumber = reversedNumber * 10 + remainder;
+        number /= 10;
+    }
+    printf("Reversed number: %d\n", reversedNumber);
+
+    return 0;
+}
